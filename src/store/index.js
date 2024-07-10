bcd976b1761049cca12955ad5afde6666b022d61
Daniela Rodríguez <32688524+RRDaniela@users.noreply.github.com>
2024-07-09 19:28:53 -0600

    ADDED: Implemented redux to change the states of our variables.

---
+++ src/store/index.js
@@ -0,0 +1,42 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const songsSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    removeSong(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
+const moviesSlice = createSlice({
+  name: "movie",
+  initialState: [],
+  reducers: {
+    addMovie(state, action) {
+      state.push(action.payload);
+    },
+    removeMovie(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    songs: songsSlice.reducer,
+    movies: moviesSlice.reducer,
+  },
+});
+
+//console.log(store.getState());
+
+export { store };
+export const { addSong, removeSong } = songsSlice.actions;
+export const { addMovie, removeMovie } = moviesSlice.actions;
